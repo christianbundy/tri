86dd0e16284332409db1cc99c1e2762b51be2194
Christian Bundy <christianbundy@fraction.io>
2021-01-06 09:23:29 -0800

    Add new implementation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tri
+
+- `node index.js`
+- arrow keys or hjkl to move
+- `i` to move **into** tree (deeper)
+- `o` to go **out** tree (shallower)
+
--- index.js
+++ index.js
@@ -1,128 +1,121 @@
 const fs = require("fs");
+const chalk = require("chalk");
+const path = require("path");
 
-const espree = require("espree");
+const data = fs
+  .readFileSync(path.join(__dirname, "package.json"))
+  .toString()
+  .split("");
 
-const code = fs.readFileSync(__filename, "utf8");
-const ast = espree.parse(code, { ecmaVersion: espree.latestEcmaVersion });
+function isSpan(a, b) {
+  return (a === " ") === (b === " ");
+}
 
-function str(node) {
-  if (node.type == null) {
-    throw new Error("Must handle one ${node[0].type} node at a time");
+function parseSpans(x) {
+  return x.reduce((acc, cur) => {
+    if (acc.length === 0) {
+      acc.push({ type: "span", value: [cur] });
+      return acc;
+    }
+
+    const lastSpan = acc[acc.length - 1].value;
+    const lastChar = lastSpan[lastSpan.length - 1];
+
+    console.log({ cur, lastChar });
+
+    if (isSpan(cur.value, lastChar.value)) {
+      acc[acc.length - 1].value.push(cur);
+    } else {
+      acc.push({ type: "span", value: [cur] });
+    }
+    return acc;
+  }, []);
+}
+
+function parseLines(x) {
+  return x.reduce(
+    (acc, cur) => {
+      if (cur == "\n") {
+        acc[acc.length - 1].value = parseSpans(acc[acc.length - 1].value);
+
+        acc.push({ type: "line", value: [] });
+      } else {
+        acc[acc.length - 1].value.push({ type: "character", value: cur });
+      }
+
+      return acc;
+    },
+    [{ type: "line", value: [] }]
+  );
+}
+
+const lines = parseLines(data);
+
+function draw(node, previous = []) {
+  if (Array.isArray(node)) {
+    node.forEach((n, i) => draw(n, [...previous, i]));
   }
 
   switch (node.type) {
-    case "TemplateLiteral":
-      const quasis = node.quasis.map(str).join("");
+    case "line":
+      draw(node.value, previous);
+      process.stdout.write("\n");
+      break;
 
-      if (node.expressions.length) {
-        const inner = node.expressions
-          .reduce(function (acc, cur, index) {
-            //            console.log('BEFORE', acc)
-            acc.splice(
-              cur.start - node.start - 3 - index,
-              0,
-              "${",
-              ...str(cur).split(""),
-              "}"
-            );
-            //           console.log('AFTER', acc)
-            return acc;
-          }, quasis.split(""))
-          .join("");
-        return `\`${inner}\``;
+    case "span":
+      draw(node.value, previous);
+      break;
+
+    case "character":
+      const hasCursor = cursor.every((x, i) => x === previous[i]);
+      if (hasCursor) {
+        process.stdout.write(chalk.bgGreen(node.value));
       } else {
-        return `\`${quasis}\``;
+        process.stdout.write(node.value);
       }
-    case "ConditionalExpression":
-      return `${str(node.test)} ? ${str(node.consequent)} : ${str(
-        node.alternate
-      )}`;
-
-    case "TemplateElement":
-      return node.value.raw;
-
-    case "Identifier":
-      return node.name;
-
-    case "Program":
-      return `${node.body.map(str).join("\n")}\n`;
-
-    case "Literal":
-      return node.raw;
-
-    case "ExpressionStatement":
-      return `${str(node.expression)};`;
-
-    case "CallExpression":
-      return `${str(node.callee)}(${node.arguments.map(str).join(", ")})`;
-
-    case "BlockStatement":
-      return `{ ${node.body.map(str).join("\n")} }`;
-
-    case "IfStatement":
-      const alternate =
-        node.alternate == null ? "" : ` else ${str(node.alternate)}`;
-      return `if (${str(node.test)}) ${str(node.consequent)}${alternate}`;
-
-    case "BinaryExpression":
-      return `${str(node.left)} ${node.operator} ${str(node.right)}`;
-
-    case "ThrowStatement":
-      return `throw ${str(node.argument)}`;
-
-    case "NewExpression":
-      return `new ${str(node.callee)}(${node.arguments.map(str).join(", ")})`;
-
-    case "SwitchStatement":
-      return `switch (${str(node.discriminant)}) {\n${node.cases
-        .map(str)
-        .join(" ")}\n}`;
-
-    case "SwitchCase":
-      const consequent = node.consequent.map(str).join("\n");
-
-      if (node.test === null) {
-        return `default:\n${consequent}\n`;
-      } else {
-        return `case ${str(node.test)}:\n${consequent}\n`;
-      }
-
-    case "ReturnStatement":
-      return `return ${str(node.argument)};\n`;
-
-    case "FunctionDeclaration":
-      const id1 = node.id === null ? "" : str(node.id);
-      return `function ${id1} (${node.params.map(str).join(", ")}) ${str(
-        node.body
-      )}`;
-
-    case "FunctionExpression":
-      const id2 = node.id === null ? "" : str(node.id);
-      return `function ${id2} (${node.params.map(str).join(", ")}) ${str(
-        node.body
-      )}`;
-
-    case "MemberExpression":
-      return `${str(node.object)}.${str(node.property)}`;
-
-    case "ObjectExpression":
-      return `{ ${node.properties.map(str).join(", ")} }`;
-
-    case "Property":
-      return `${str(node.key)}: ${str(node.value)}`;
-
-    case "VariableDeclaration":
-      return `${node.kind} ${node.declarations.map(str).join(", ")};`;
-
-    case "VariableDeclarator":
-      return `${str(node.id)} = ${str(node.init)}`;
-
-    case "SpreadElement":
-      return `...${str(node.argument)}`;
-
-    default:
-      throw node;
+      break;
   }
 }
 
-process.stdout.write(str(ast));
+const cursor = [0];
+draw(lines);
+
+const readline = require("readline");
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+process.stdin.on("keypress", (str, key) => {
+  process.stdin.write("\033[2J");
+  switch (key.name) {
+    case "escape":
+    case "c":
+      process.exit(0);
+      break;
+    case "o":
+      cursor.pop();
+      draw(lines);
+      break;
+    case "i":
+      cursor.push(0);
+      draw(lines);
+      break;
+    case "up":
+    case "left":
+    case "h":
+    case "k":
+      cursor[cursor.length - 1] -= 1;
+      draw(lines);
+      break;
+    case "down":
+    case "right":
+    case "l":
+    case "j":
+      cursor[cursor.length - 1] += 1;
+      draw(lines);
+      break;
+
+    default:
+      console.log(key);
+  }
+});
